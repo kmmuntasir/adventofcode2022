0428b92bfc99525b7b1ac182ad0ef77a44a99303
Muntasir Billah Munna <kmmuntasir@gmail.com>
2022-12-02 16:15:13 +0100

    Update the solution for Day 01

--- day01/solution.cpp
+++ day01/solution.cpp
@@ -2,53 +2,30 @@
 
 using namespace std;
 
-int main()
-{
-    string input = "<inputfile>";
-    
-    char c = 'A';
-    char current[20] = "";
-    int curlen = 0;
-    int i = 0;
+int main() {
+
+    freopen("input.txt", "r", stdin);
+
+    string number;
     int maxx=0, sum=0;
     vector<int> totals ;
-    
-    while(c != '\0') {
-        c = input[i];
-        if(c == '\n') {
-            if(0 < strlen(current)) {
-                current[curlen] = '\0';
-                cout << current << " ";
-                sum += atoi(current);
-                cout << sum << endl;
-                current[0] = '\0';
-                curlen = 0;
-            } else {
-                maxx = sum > maxx ? sum : maxx;
-                totals.push_back(sum);
-                sum = 0;
-            }
+
+    while(getline(cin, number)) {
+        if(number[0] == '\0') {
+            maxx = max(sum, maxx);
+            totals.push_back(sum);
+            sum = 0;
         } else {
-            current[curlen++] = c;
+            sum += stoi(number);
         }
-        i++;
     }
-    
-    cout << endl << endl << maxx << endl;
-    
+
     sort(totals.begin(), totals.end());
-    reverse(totals.begin(), totals.end());
-    
-    
-    cout << endl << totals[0];
-    cout << endl << totals[1];
-    cout << endl << totals[2];
-    cout << endl;
-    
-    int threesum = totals[0] + totals[1] + totals[2];
-    
-    cout << threesum << endl;
+
+    int topThreeTotal = totals[totals.size() - 1] + totals[totals.size() - 2] + totals[totals.size() - 3];
+
+    cout << "Max: " << maxx << endl;
+    cout << "Top Three Total: " << topThreeTotal << endl;
 
     return 0;
 }
-
